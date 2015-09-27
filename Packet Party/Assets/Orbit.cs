4fa633be4f045595425f4c2b1763f05b360a2b2c
trixyrabbit@gmail.com <trixyrabbit@gmail.com>
2015-09-27 04:40:28 -0400

    adding unity scripts

---
+++ Packet Party/Assets/Orbit.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbit : MonoBehaviour {
+		
+	public Transform packet;
+	public int numPackets;
+	public int radius = 1;
+	//public Transform[] packets;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.position = new Vector3 (transform.position.x,,transform.position.z);
+		if (transform.childCount != numPackets) {
+			Transform[] children = transform.GetComponentsInChildren<Transform>();
+			foreach (Transform ch in children){
+				if(ch != transform){
+					Destroy(ch.gameObject);
+				}
+			}
+			//packets = new Transform[numPackets];
+
+			for (int i = 0; i < numPackets; i++) {
+				float angle = i * Mathf.PI * 2 / numPackets;
+				Vector3 pos = new Vector3 (Mathf.Cos (angle), 0, Mathf.Sin (angle)) * radius + transform.position;
+				Transform t = Instantiate (packet, pos, Quaternion.identity) as Transform;
+				t.parent = transform;
+
+				Debug.Log ("DOT");
+				//t.gameObject.AddComponent<OrbiterMove>().center = this.transform;
+				//packets[i].gameObject.AddComponent<OrbiterMove>().rotationSpeed = 0;
+			}
+		}
+	}
+}
---
+++ Packet Party/Assets/OrbiterMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbiterMove : MonoBehaviour {
+	public Vector3 axis = Vector3.up; //rotate around Z
+	public Vector3 newPos; //the next move location
+	public float radius = 1.0f, radiusSpeed = 88f, rotationSpeed = 48.0f; //params
+
+
+	// Use this for initialization
+	void Start () {
+		//transform.position = (transform.position - center.position).normalized * radius + center.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (transform.parent.position,axis,rotationSpeed*Time.deltaTime);
+		newPos = (transform.position - transform.parent.position).normalized * radius + transform.parent.position;
+		transform.position = Vector3.MoveTowards (transform.position,newPos,Time.deltaTime*radiusSpeed);
+	}
+}
